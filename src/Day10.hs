2274fd64a7cc037d1e465eeb9442616873fb1642
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-08 23:03:18 +0000

    adds day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -3,6 +3,10 @@ module Day10 (
 ) where
 
 import Common
+import Control.Arrow ((&&&))
+import Data.List (group)
 
 day10 :: AOCSolution
-day10 input = ["", ""]
+day10 input = show . length . (iterate go input !!) <$> [40, 50]
+  where
+    go = concatMap (uncurry (++) . (show . length &&& take 1)) . group
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -2,13 +2,13 @@ module Day10Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "1"
 
 spec :: Spec
 spec = describe "Day 10" $ do
   it "Sample" $ do
-    day10 testInput `shouldBe` ["", ""]
+    day10 testInput `shouldBe` ["82350", "1166642"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day10.txt"
-    day10 actualInput `shouldBe` ["", ""]
+    day10 actualInput `shouldBe` ["492982", "6989950"]
