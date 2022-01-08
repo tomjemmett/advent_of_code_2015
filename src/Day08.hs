2c702b380f33d09de126b05b6a492ca5f131c5ee
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-08 22:30:20 +0000

    adds day 8

--- src/Day08.hs
+++ src/Day08.hs
@@ -3,6 +3,24 @@ module Day08 (
 ) where
 
 import Common
+import Control.Arrow ((&&&))
 
 day08 :: AOCSolution
-day08 input = ["", ""]
+day08 input = show . uncurry (-) <$> ([part1, part2] <*> pure (lines input))
+
+part1 :: [String] -> (Int, Int)
+part1 = sum . map length &&& sum . map p
+  where
+    p [] = -2
+    p ('\\':'\\':xs) = 1 + p xs
+    p ('\\':'\"':xs) = 1 + p xs
+    p ('\\':'x':_:_:xs) = 1 + p xs
+    p (_:xs) = 1 + p xs
+
+part2 :: [String] -> (Int, Int)
+part2 = sum . map p &&& sum . map length
+  where
+    p [] = 2
+    p ('\\':xs) = 2 + p xs
+    p ('"':xs) = 2 + p xs
+    p (_:xs) = 1 + p xs
--- test/Day08Spec.hs
+++ test/Day08Spec.hs
@@ -2,13 +2,17 @@ module Day08Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "\"\"\n\
+            \\"abc\"\n\
+            \\"aaa\\\"aaa\"\n\
+            \\"\\x27\""
+
 
 spec :: Spec
 spec = describe "Day 08" $ do
   it "Sample" $ do
-    day08 testInput `shouldBe` ["", ""]
+    day08 testInput `shouldBe` ["12", "19"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day08.txt"
-    day08 actualInput `shouldBe` ["", ""]
+    day08 actualInput `shouldBe` ["1371", "2117"]
