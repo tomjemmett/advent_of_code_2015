da46225d71a72771f371aafbc4a71ae6e41a738b
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-08 22:30:27 +0000

    adds day 9

--- src/Day09.hs
+++ src/Day09.hs
@@ -3,6 +3,38 @@ module Day09 (
 ) where
 
 import Common
+import Control.Monad (guard)
+import Data.Maybe (fromMaybe)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Text.Parsec as P
+
+type Edge  = (Int, String)
+type Edges = [Edge] 
+type Graph = M.Map String Edges
 
 day09 :: AOCSolution
-day09 input = ["", ""]
+day09 input = show <$> ([minimum, maximum] <*> pure p)
+  where
+    i = parseInput input
+    p = map sum $ map2 fst $ paths i
+
+parseInput :: String -> Graph
+parseInput = M.fromListWith (++) . concatMap (parse' p id) . lines
+  where
+    p = do
+      source <- P.many1 P.letter <* P.string " to "
+      target <- P.many1 P.letter <* P.string " = "
+      weight <- number
+      return [(source, [(weight, target)]), (target, [(weight, source)])]
+
+paths :: Graph -> [Edges]
+paths graph = concatMap (walk S.empty 0 . (0,)) $ M.keys graph
+  where
+    n' = M.size graph
+    walk _    n (w, "") = [[(w, "")] | n == n']
+    walk seen n (w,  s) = do
+      guard $ not $ S.member s seen
+      neighbour <- M.findWithDefault [] s graph ++ [(0, "")]
+      next <- walk (S.insert s seen) (succ n) neighbour
+      return $ (w, s):next
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -2,13 +2,15 @@ module Day09Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "London to Dublin = 464\n\
+            \London to Belfast = 518\n\
+            \Dublin to Belfast = 141"
 
 spec :: Spec
 spec = describe "Day 09" $ do
   it "Sample" $ do
-    day09 testInput `shouldBe` ["", ""]
+    day09 testInput `shouldBe` ["605", "982"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day09.txt"
-    day09 actualInput `shouldBe` ["", ""]
+    day09 actualInput `shouldBe` ["251", "898"]
