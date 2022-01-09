666967be37287ed86ce90e6f00d076c15aa7b5ea
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-09 22:05:25 +0000

    adds day 14

--- src/Day14.hs
+++ src/Day14.hs
@@ -3,6 +3,22 @@ module Day14 (
 ) where
 
 import Common
+import Data.List (transpose)
 
 day14 :: AOCSolution
-day14 input = ["", ""]
+day14 input = show . maximum <$> [p1, p2]
+  where
+    i = map go $ parseInput input
+    p1 = maximum i
+    p2 = map sum $ transpose $ map score $ transpose i
+
+parseInput :: String -> [(String, Int, Int, Int)]
+parseInput = map (p . words) . lines
+  where
+    p (a:_:_:d:_:_:t:_:_:_:_:_:_:r:_) = (a, read d, read t, read r)
+
+score :: [Int] -> [Int]
+score i = map (fromEnum . (== maximum i)) i
+
+go :: (String, Int, Int, Int) -> [Int]
+go (_, d, t, r) = tail $ scanl (+) 0 $ take 2503 $ cycle $ replicate t d ++ replicate r 0
--- test/Day14Spec.hs
+++ test/Day14Spec.hs
@@ -2,13 +2,14 @@ module Day14Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\n\
+            \Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
 
 spec :: Spec
 spec = describe "Day 14" $ do
   it "Sample" $ do
-    day14 testInput `shouldBe` ["", ""]
+    day14 testInput `shouldBe` ["2640", "1564"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day14.txt"
-    day14 actualInput `shouldBe` ["", ""]
+    day14 actualInput `shouldBe` ["2548", "1102"]
