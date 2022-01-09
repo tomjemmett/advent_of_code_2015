31ab10ba605e1b4e6934d72491ad3cffe1b132f9
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-09 21:26:27 +0000

    adds day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -3,6 +3,35 @@ module Day13 (
 ) where
 
 import Common
+import Control.Arrow ((&&&))
+import Data.List (nub, permutations)
+import Data.Tuple (swap)
+import qualified Data.Map as M
 
 day13 :: AOCSolution
-day13 input = ["", ""]
+day13 input = show . go <$> [p1, p2]
+  where
+    p1 = parseInput input
+    p2 = addMe p1
+
+people :: M.Map (String, String) Int -> [String]
+people i = nub $ map fst $ M.keys i
+
+addMe :: M.Map (String, String) Int -> M.Map (String, String) Int
+addMe i = M.union i $ M.fromList $ map (,0) ([id, swap] <*> map ("me", ) (people i))
+
+go :: M.Map (String, String) Int -> Int
+go i = maximum $ map sn $ permutations $ people i
+  where
+    sn ns = sum $ map s (zip3 ns' (tail ns') (tail $ tail ns'))
+      where
+        ns' = take (length ns + 2) $ cycle ns
+        s (l,x,r) = i M.! (x,l) + i M.! (x,r)
+
+parseInput :: String -> M.Map (String, String) Int
+parseInput = M.fromList . map parseLine . lines
+
+parseLine :: String -> ((String, String), Int)
+parseLine = (\(p:_:t:h:_:_:_:_:_:_:[q]) -> ((p, init q), f t h)) . words
+  where
+    f t h = if t == "gain" then read h else (-(read h))
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -2,13 +2,24 @@ module Day13Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "Alice would gain 54 happiness units by sitting next to Bob.\n\
+            \Alice would lose 79 happiness units by sitting next to Carol.\n\
+            \Alice would lose 2 happiness units by sitting next to David.\n\
+            \Bob would gain 83 happiness units by sitting next to Alice.\n\
+            \Bob would lose 7 happiness units by sitting next to Carol.\n\
+            \Bob would lose 63 happiness units by sitting next to David.\n\
+            \Carol would lose 62 happiness units by sitting next to Alice.\n\
+            \Carol would gain 60 happiness units by sitting next to Bob.\n\
+            \Carol would gain 55 happiness units by sitting next to David.\n\
+            \David would gain 46 happiness units by sitting next to Alice.\n\
+            \David would lose 7 happiness units by sitting next to Bob.\n\
+            \David would gain 41 happiness units by sitting next to Carol."
 
 spec :: Spec
 spec = describe "Day 13" $ do
   it "Sample" $ do
-    day13 testInput `shouldBe` ["", ""]
+    day13 testInput `shouldBe` ["330", "286"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day13.txt"
-    day13 actualInput `shouldBe` ["", ""]
+    day13 actualInput `shouldBe` ["664", "640"]
