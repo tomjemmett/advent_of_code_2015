a91b0e41827648eea8281a0ef13b309ecd364fd0
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-09 23:56:42 +0000

    adds day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -3,6 +3,42 @@ module Day16 (
 ) where
 
 import Common
+import Control.Arrow (second)
+import Data.List.Split (splitOn)
+import qualified Data.Map as M
 
 day16 :: AOCSolution
-day16 input = ["", ""]
+day16 input = show <$> (go <$> [valid1, valid2] <*> pure 1 <*> pure (parseInput input))
+
+valid1 :: M.Map String (Int -> Bool)
+valid1 = M.fromList [("children", (== 3))
+                    ,("cats", (== 7))
+                    ,("samoyeds", (== 2))
+                    ,("pomeranians", (== 3))
+                    ,("akitas", (== 0))
+                    ,("vizslas", (== 0))
+                    ,("goldfish", (== 5))
+                    ,("trees", (== 3))
+                    ,("cars", (== 2))
+                    ,("perfumes", (== 1))]
+
+valid2 :: M.Map String (Int -> Bool)
+valid2 = foldr (uncurry M.insert) valid1 x
+  where
+    x = [("cats", (> 7)), ("pomeranians", (< 3)), ("goldfish", (< 5)), ("trees", (> 3))]
+
+
+parseInput :: String -> [[(String, Int)]]
+parseInput = map f . lines
+  where
+    f = map (second read . tuplify2 . splitOn ": " . tail) .
+      splitOn "," .
+      tail .
+      dropWhile (/= ':')
+
+go :: M.Map String (Int -> Bool) -> Int -> [[(String, Int)]] -> Int
+go valid n (sue:sues) = if all f sue
+    then n
+    else go valid (succ n) sues
+  where
+    f (k, v) = (valid M.! k) v
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -2,13 +2,10 @@ module Day16Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 16" $ do
-  it "Sample" $ do
-    day16 testInput `shouldBe` ["", ""]
-      
+  -- no sample
+
   it "Actual" $ do
     actualInput <- readFile "inputs/day16.txt"
-    day16 actualInput `shouldBe` ["", ""]
+    day16 actualInput `shouldBe` ["40", "241"]
