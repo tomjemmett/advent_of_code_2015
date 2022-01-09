584250e80ce11679c8573a3f02bcd8ed110615cd
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-09 16:16:23 +0000

    adds solution for day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -3,6 +3,31 @@ module Day12 (
 ) where
 
 import Common
+import Control.Lens ((^?!))
+import Data.Aeson (Value(Array, Number, Object), Object, toJSON)
+import Data.Aeson.Lens (_Value)
+import Data.Scientific (Scientific, toRealFloat)
+import Data.Aeson.KeyMap (toList)
 
 day12 :: AOCSolution
-day12 input = ["", ""]
+day12 input = show . round . toRealFloat . sum <$> ([getNumbers, getNonRedNumbers] <*> pure (parseInput input))
+
+parseInput :: String -> Value
+parseInput = (^?! _Value)
+
+getNumbers :: Value -> [Scientific]
+getNumbers = \ case
+  Number n -> [n]
+  Object o -> foldMap getNumbers o
+  Array  a -> foldMap getNumbers a
+  _        -> []
+
+getNonRedNumbers :: Value -> [Scientific]
+getNonRedNumbers = \case
+    Number n                       -> [n]
+    Object o | not $ containsRed o -> foldMap getNonRedNumbers o
+    Array  a                       -> foldMap getNonRedNumbers a
+    _                              -> []
+
+containsRed :: Object -> Bool
+containsRed o = elem (toJSON "red") $ map snd $ toList o
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -2,13 +2,13 @@ module Day12Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "[1,{\"c\":\"red\",\"b\":2},3]"
 
 spec :: Spec
 spec = describe "Day 12" $ do
   it "Sample" $ do
-    day12 testInput `shouldBe` ["", ""]
+    day12 testInput `shouldBe` ["6", "4"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day12.txt"
-    day12 actualInput `shouldBe` ["", ""]
+    day12 actualInput `shouldBe` ["191164", "87842"]
