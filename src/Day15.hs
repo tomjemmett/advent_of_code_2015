f3f24b53013044888009b0e4fc935f8ef5d12102
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-09 23:20:14 +0000

    adds day 15

--- src/Day15.hs
+++ src/Day15.hs
@@ -3,6 +3,37 @@ module Day15 (
 ) where
 
 import Common
+import Control.Monad (guard)
+import Data.List (transpose)
+import Data.List.Split (splitOn)
 
 day15 :: AOCSolution
-day15 input = ["", ""]
+day15 input = show . maximum <$> ([part1, part2] <*> pure i)
+  where
+    i = parseInput input
+
+parseInput :: String -> [[Int]]
+parseInput = map p . lines
+  where
+    p = map (read . last . words) . splitOn "," . dropWhile (/= ' ')
+
+part1 :: [[Int]] -> [Int]
+part1 i = do
+  w <- [0..100]
+  x <- [0..100]
+  y <- [0..100]
+  guard $ w + x + y <= 100
+  let z = 100 - w - x - y
+      r = map (max 0 . sum) $ transpose $ zipWith (\a b -> (a*) <$> b) [w, x, y, z] i
+  return (product $ init r)
+
+part2 :: [[Int]] -> [Int]
+part2 i = do
+  w <- [0..100]
+  x <- [0..100]
+  y <- [0..100]
+  guard $ w + x + y <= 100
+  let z = 100 - w - x - y
+      r = map (max 0 . sum) $ transpose $ zipWith (\a b -> (a*) <$> b) [w, x, y, z] i
+  guard $ last r == 500
+  return (product $ init r)
--- test/Day15Spec.hs
+++ test/Day15Spec.hs
@@ -2,13 +2,16 @@ module Day15Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8\n\
+            \Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3\n\
+            \skip1: capacity 0, durability 0, flavor 0, texture 0, calories 0\n\
+            \skip2: capacity 0, durability 0, flavor 0, texture 0, calories 0"
 
 spec :: Spec
 spec = describe "Day 15" $ do
   it "Sample" $ do
-    day15 testInput `shouldBe` ["", ""]
+    day15 testInput `shouldBe` ["62842880", "57600000"]
       
   it "Actual" $ do
     actualInput <- readFile "inputs/day15.txt"
-    day15 actualInput `shouldBe` ["", ""]
+    day15 actualInput `shouldBe` ["13882464", "11171160"]
