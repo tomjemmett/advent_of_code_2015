e9635a608e6ee5526c1968e1e16f3cc9006b2f62
Tom Jemmett <tom.jemmett@gmail.com>
2022-01-13 22:57:48 +0000

    adds day 20

--- src/Day20.hs
+++ src/Day20.hs
@@ -3,6 +3,22 @@ module Day20 (
 ) where
 
 import Common
+import Data.List (nub, findIndex)
+import Data.Maybe (fromJust)
 
 day20 :: AOCSolution
-day20 input = ["", ""]
+day20 input = show . fromJust . findIndex (>= i) . go <$> [p1, p2]
+  where
+    i = parseInput input
+    go (x, f) = map (\n -> (x *) . sum . filter (f n) . factors $ n) [0..]
+    p1 = (10, const $ const True)
+    p2 = (11, \n -> (> (n - 1) `div` 50))
+
+parseInput :: String -> Int
+parseInput = read
+
+isqrt :: Int -> Int
+isqrt = floor . sqrt . fromIntegral
+
+factors :: Int -> [Int]
+factors n = nub . concat $ [[x, q] | x <- [1..isqrt n], let (q, r) = divMod n x, r == 0]
--- test/Day20Spec.hs
+++ test/Day20Spec.hs
@@ -2,13 +2,8 @@ module Day20Spec ( spec ) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 20" $ do
-  it "Sample" $ do
-    day20 testInput `shouldBe` ["", ""]
-      
   it "Actual" $ do
     actualInput <- readFile "inputs/day20.txt"
-    day20 actualInput `shouldBe` ["", ""]
+    day20 actualInput `shouldBe` ["665280", "705600"]
